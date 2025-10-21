a88749ad11d6d9d1478f13622e5f2f09e6174dd2
Viktoriia Kyrylishyna <vkyryli1@stu.vistula.edu.pl>
2025-10-21 13:49:13 +0200

    function for Cancel button

--- frontend/public_page/public_page_script.js
+++ frontend/public_page/public_page_script.js
@@ -3,10 +3,12 @@ const signUpBtn = document.getElementById('sign_up_btn');
 const logInBtn = document.getElementById('log_in_btn');
 const closeModal = document.getElementById('closeModal');
 const getStartedBtn = document.getElementById('get_started_btn');
-
+const cancelBtnLogin = document.getElementById('cancelBtnLogin');
+const cancelBtnSignup = document.getElementById('cancelBtnSignup');
 const signupForm = document.getElementById('signup_form');
 const loginForm = document.getElementById('login_form');
 const switchToLogin = document.getElementById('switchToLogin');
+const switchToSignup = document.getElementById('switchToSignUp');
 
 signUpBtn.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -35,3 +37,15 @@ switchToLogin.addEventListener('click', () => {
     loginForm.classList.remove('hidden');
 });
 
+switchToSignup.addEventListener('click', () => {
+    signupForm.classList.remove('hidden');
+    loginForm.classList.add('hidden');
+});
+
+cancelBtnLogin.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+cancelBtnSignup.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
